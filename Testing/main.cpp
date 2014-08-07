80a91bb59c7c928b2aab0ca0c556433d9b837a36
Ze Li <zeli@Zes-MacBook-Pro.local>
2014-08-06 22:23:39 -0500

    Initial Commit

---
+++ Testing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Testing
+//
+//  Created by Ze Li on 8/6/14.
+//  Copyright (c) 2014 Ze. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
